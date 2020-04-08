919c166839a84bc0d9d9e6c569b51a958a6bd8c4
Yana <yakaz36174@gmail.com>
2020-04-08 15:57:30 +0300

    Настраивала плагин для ringe slider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import './style.scss'
 import './components/buttons/index'
 window.jQuery = window.$ = require('./scripts/jquery-3.4.1.min.js');
-require('./scripts/nativemultiple.jquery.min.js')
-import './styles/slider.css'
+require('./scripts/jquery-ui.min.js')
+require('./scripts/jquery.ui.touch-punch.min.js')
 
-// console.log($('.title'))
-
-$('input.slider').nativeMultiple({
-        stylesheet: "slider",
-        onCreate: function() {
-            console.log(this);
-        },
-        onChange: function(first_value, second_value) {
-            console.log('onchange', [first_value, second_value]);
-        },
-        onSlide: function(first_value, second_value) {
-            console.log('onslide', [first_value, second_value]);
-        }
-    });
+$(".polzunok-5").slider({
+    min: 0,
+    max: 5000,
+    values: [2000, 3000],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {    
+        $(".polzunok-input-5-left").val(ui.values[ 0 ]);   
+        $(".polzunok-input-5-right").val(ui.values[ 1 ]);  
+    }    
+});
+$(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
+$(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
+$(document).focusout(function() {
+    var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),    
+    opt_left = $(".polzunok-5").slider("option", "min"),
+    where_right = $(".polzunok-5").slider("values", 1),
+    input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),    
+    opt_right = $(".polzunok-5").slider("option", "max"),
+    where_left = $(".polzunok-5").slider("values", 0); 
+    if (input_left > where_right) { 
+        input_left = where_right; 
+    }
+    if (input_left < opt_left) {
+        input_left = opt_left; 
+    }
+    if (input_left == "") {
+    input_left = 0;    
+    }        
+    if (input_right < where_left) { 
+        input_right = where_left; 
+    }
+    if (input_right > opt_right) {
+        input_right = opt_right; 
+    }
+    if (input_right == "") {
+    input_right = 0;    
+    }    
+    $(".polzunok-input-5-left").val(input_left); 
+    $(".polzunok-input-5-right").val(input_right); 
+    $(".polzunok-5").slider( "values", [ input_left, input_right ] );
+});
